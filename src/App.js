1b5538b56b270c070a3636b9d3141ac845240dfe
sg-lsm <ckdgmlrt@gmail.com>
2022-07-28 13:04:28 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      await axios
+        .get("https://jsonplaceholder.typicode.com/todos/1")
+        .then((res) => {
+          setData(res.data);
+        });
+    } catch (e) {
+      throw e;
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
